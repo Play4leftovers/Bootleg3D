18a2bbbaa9250e8093ca50e06a2ca41ec9cb9ee6
Tor Nordmark <tornordmark@hotmail.com>
2022-10-07 11:47:55 +0200

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -13,7 +14,11 @@ public class GameManager : MonoBehaviour
     List<TeamStruct> Teams;
     public GameObject Worm;
 
-    private int _turnNumber;
+    private int _turnNumber = 0;
+    private int _wormNumber = 0;
+
+    public CinemachineVirtualCamera AimCam;
+    public CinemachineVirtualCamera TPSCam;
 
     private void Awake()
     {
@@ -56,19 +61,23 @@ public class GameManager : MonoBehaviour
         for(int i = 0; i < _numberOfTeams; i++)
         {
             TeamStruct _tempTeam = new(_numberOfWorms, i+1);
+            Color _teamColor = new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value, 1.0f);
+
             for(int x = 0; x < _numberOfWorms; x++)
             {
                 Vector3 _randSpawnPos = new(UnityEngine.Random.Range(-50, 51), 5, UnityEngine.Random.Range(-50, 51));
                 _tempTeam.TeamMembers[x] = Instantiate(Worm, _randSpawnPos, Quaternion.identity);
+                _tempTeam.TeamMembers[x].GetComponentInChildren<Renderer>().material.color = _teamColor;
             }
 
             Teams.Add(_tempTeam);
         }
+        UpdateGameState(GameState.PlayerTurn);
     }
 
     private void PlayerVictory()
     {
-        throw new NotImplementedException();
+        
     }
 
     private void NewTurn()
@@ -78,7 +87,12 @@ public class GameManager : MonoBehaviour
         //Add Cameras to its variables so it follows correctly
         //When has fired, end turn and make a new turn
         //When no more of one team exists, call upon PlayerVictory
-        throw new NotImplementedException();
+        GameObject _currWorm = Teams[_turnNumber].TeamMembers[_wormNumber];
+        TPSCam.m_LookAt = _currWorm.transform;
+        AimCam.m_LookAt = _currWorm.transform;
+
+        TPSCam.m_Follow = _currWorm.transform;
+        AimCam.m_Follow = _currWorm.transform;
     }
 }
 
